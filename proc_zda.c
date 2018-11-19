23d260429cea7d3455bbf27207aa09c9d024f2d0
buyi97 <44101602+buyi97@users.noreply.github.com>
2018-11-19 11:29:06 -0300

    Corregido proc_zda

--- proc_zda.c
+++ proc_zda.c
@@ -22,67 +22,53 @@
 #define NMEA_ZDA_MMUTC_POS	6
 
 
-
 status_t proc_zda(nmea_t* nmea, char* cadena){
 
 	char* cad_arr[NMEA_DELIM_CANT_MAX];
 	size_t cant;
-	status_t st;
-	long int ntemp
 	char * temp;
 
 	if ( !cadena || !nmea )
 		return ST_ERR_PUNT_NULL;
 
-    /* split devuelve la cant de cadenas que separo. Si ese numero es menor a
-       NMEA_DELIM_CANT_MIN, se descarta el dato*/
+    /* split devuelve la cant de cadenas que separo. Si ese numero es menor a NMEA_DELIM_CANT_MIN, se descarta el dato*/
 	if ( split_delim( cadena , cad_arr , NMEA_DELIM_CHAR ) < NMEA_DELIM_CANT_MIN )
 		return ST_ERR_SENTENCIA_INVALIDA;
-
-	if ( ( st = str2hora( cad_arr[NMEA_ZDA_HORA_POS] , &nmea->zda.hora ) ) != ST_OK )
+	
+	/* convierto de cadena "hhmmss.ss" al tipo hora_t (struct hora) con la función str2hora (fecha.h) */
+	if ( str2hora( cad_arr[NMEA_ZDA_HORA_POS] , &nmea->zda.hora ) != ST_OK )
 		return ST_ERR_SENTENCIA_INVALIDA;
 
-    //if ( (st = str2fecha( cad_arr[NMEA_ZDA_HORA_POS] , &nmea->zda.fecha ) ) != ST_OK )
-    //    return ST_ERR_SENTENCIA_INVALIDA;
-
-	/* verifico que el dia sea un numero y si lo es lo cargo en la estructura */
-	ntemp = strtol(cad_arr[NMEA_ZDA_DIA_POS], &temp, 10);
-	if ( *temp != '\0' ) {
+	/* verifico que el dia sea un numero y lo cargo en la estructura */
+	nmea->zda.fecha.day = strtoul(cad_arr[NMEA_ZDA_DIA_POS], &temp, 10);
+	
+	if ( *temp != '\0' )
 		return ST_ERR_SENTENCIA_INVALIDA;
-	} else {
-		ntemp = nmea->zda.fecha.day;
-	}
 
 	/* idem anterior para el mes */
-	ntemp = strtol(cad_arr[NMEA_ZDA_MES_POS], &temp, 10);
-	if ( *temp != '\0' ) {
+	nmea->zda.fecha.month = strtoul(cad_arr[NMEA_ZDA_MES_POS], &temp, 10);
+	
+	if ( *temp != '\0' )
 		return ST_ERR_SENTENCIA_INVALIDA;
-	} else {
-		ntemp = nmea->zda.fecha.month;
-	}
 
 	/* idem anterior para el año */
-	ntemp = strtol(cad_arr[NMEA_ZDA_ANIO_POS], &temp, 10);
-	if ( *temp != '\0' ) {
+	nmea->zda.fecha.year = strtoul(cad_arr[NMEA_ZDA_ANIO_POS], &temp, 10);
+	
+	if ( *temp != '\0' )
 		return ST_ERR_SENTENCIA_INVALIDA;
-	} else {
-		ntemp = nmea->zda.fecha.year;
-	}
 
-	/* verifico que zona horaria sea un numero y si lo es lo cargo en la estructura */
-	ntemp = strtol(cad_arr[NMEA_ZDA_HHUTC_POS], &temp, 10);
-	if ( *temp != '\0' ) {
+	/* verifico que zona horaria sea un numero y lo cargo en la estructura */
+	nmea->zda.hh_utc = strtol(cad_arr[NMEA_ZDA_HHUTC_POS], &temp, 10);
+	
+	if ( *temp != '\0' )
 		return ST_ERR_SENTENCIA_INVALIDA;
-	} else {
-		ntemp = nmea->zda.hh_utc;
-	}
 
 	/* idem anterior pero para los minutos de diferencia */
-	ntemp = strtol(cad_arr[NMEA_ZDA_HHUTC_POS], &temp, 10);
-	if ( *temp != '\0' ) {
+	nmea->zda.mm_utc = strtol(cad_arr[NMEA_ZDA_HHUTC_POS], &temp, 10);
+	
+	if ( *temp != '\0' )
 		return ST_ERR_SENTENCIA_INVALIDA;
-	} else {
-		ntemp = nmea->zda.mm_utc;
-	}
+	
+	return ST_OK;
 
 }
