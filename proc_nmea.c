36399b10923cf33b3cdb85457cc5d73f586b57b6
buyi97 <44101602+buyi97@users.noreply.github.com>
2018-11-19 11:50:20 -0300

    Actualizado proc_nmea
    La funcion crea por memoria dinamica un tipo nmea_t y le carga los datos
    Si todo salió bien, devuelve ST_OK y la estructura nmea toda cargada

--- proc_nmea.c
+++ proc_nmea.c
@@ -11,30 +11,38 @@
 
 
 /**
-* @brief Procesa una sentencia NMEA
-* @param Cadena NMEA (const)
+* @brief Procesa una sentencia NMEA, creando por memoria dinámica la nmea_t
+* @param Cadena NMEA (const), puntero doble a nmea_t
 * @return El estado en el que terminó (ST_OK si todo fue bien)
 */
 
 
-status_t proc_nmea( char* cadena ){
+status_t proc_nmea( char* cadena , nmea_t ** nmea ){
 	
-	nmea_t nmea;
 	status_t st;
 	status_t (*pfunc[ NMEA_CANT_TIPOS ])( nmea_t* , char* ) = { proc_rmc,  proc_zda,  proc_gga }; /*Puntero a funciones*/
 	
-	if ( !cadena )
+	if ( !cadena || !nmea )
 		return ST_ERR_PUNT_NULL;
 	
 	if ( verify_checksum( cadena ) == false )
 		return ST_ERR_SENTENCIA_INVALIDA;
 	
-	if ( ( st = get_nmea_id( cadena , &nmea.id ) ) != ST_OK )
-		return st;
+	if (  !( *nmea = (nmea_t*)calloc(1,sizeof(nmea_t)) )
+	    	return ST_ERR_NOMEM;
 	
-	if ( ( st = (*pfunc[ nmea.id ])( &nmea , cadena ) ) != ST_OK )
+	if ( ( st = get_nmea_id( cadena , &((*nmea)->id) ) ) != ST_OK ){
+		free(*nmea);
+		*nmea=NULL;
 		return st;
+	}
 	
-	/*FALTA TERMINAR LA FUNCION, Y TERMINAR DE CERRAR LA IDEA*/
+	if ( ( st = (*pfunc[ (*nmea)->id) ])( *nmea , cadena ) ) != ST_OK ){
+		free(*nmea);
+		*nmea=NULL;
+		return st;
+	}
+	
+	return ST_OK;
 	
 }
