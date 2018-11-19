5fb349adfc5efb22d5ef6ee1e707c83afe42ab84
patovermal <44077261+patovermal@users.noreply.github.com>
2018-11-18 23:43:04 -0300

    Cambio en notacion de ptr2ptrarray, se usa [i] en vez de [0] o [1]

--- split_delim.c
+++ split_delim.c
@@ -1,10 +1,3 @@
-/**
-* @file split_delim.c
-* @Author buyi97
-* @date 17/11/2019
-* @brief Separar una cadena en varias a partir de un caracter delimitador
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -16,27 +9,28 @@
 */
 
 size_t split_delim (char * cadena, char ** ptr2ptrarray, char delim) {
-	
+
 	size_t i=0;	/* variable de iteracion */
 
 	if ( !cadena || !ptr2ptrarray )	/* valido los punteros */
 		return 0;
-	
-	ptr2ptrarray[0] = cadena;
-	
-	if ( !( ptr2ptrarray[1] = strchr(cadena,delim) ) )	/* busco el primer delim */
+
+	ptr2ptrarray[i] = cadena;
+	i++;
+
+	if ( !( ptr2ptrarray[i] = strchr(cadena,delim) ) )	/* busco el primer delim */
 		return 0;
+
+	ptr2ptrarray[i][0] = '\0';	/* le asigno un \0 */
+	ptr2ptrarray[i]++;			/* le hago apuntar a un lugar despues del \0 */
+	i++;
 	
-	*(ptr2ptrarray[1])='\0';	/* le asigno un \0 */
-	ptr2ptrarray[1]++;			/* le hago apuntar a un lugar despues del \0 */
-	
-	for (i = 2; ( ptr2ptrarray[i] = strchr(ptr2ptrarray[i-1],delim) ) ; i++) {
-		
+	for (; ( ptr2ptrarray[i] = strchr(ptr2ptrarray[i-1],delim) ) ; i++) {
+
 		ptr2ptrarray[i][0] = '\0';
 		ptr2ptrarray[i]++;
-		
-	}
 
+	}
 	return i;
-	
+
 }
