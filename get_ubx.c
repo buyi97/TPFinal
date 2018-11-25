fae7cf05155b6d808615193e072c3269717ac452
Mauro Arbuello <43795320+mauroarbuello@users.noreply.github.com>
2018-11-25 13:14:09 -0300

    Se cambió todo exepto el nombre

--- get_ubx.c
+++ get_ubx.c
@@ -5,50 +5,24 @@
 #include "status.h"
 #include "estructuras.h"
 
-#define STR_LEN 400 /*La longitúd del arreglo debe ser mayor a la máxima longitud posible para una sentencia UBX*/
-#define SYNC_CHARS "µb" /*caracteres de sincronismo en formato ASCII*/ 
+#define BUFFER_LEN 400 /*La longitúd del arreglo debe ser mayor a la máxima longitud posible para una sentencia UBX*/
+#define SYNC_CHAR1 0xB5
+#define SYNC_CHAR2 0X62
 #define SHIFT_BYTE 8
-#define SIZE_OF_SYNC_CHARS 2
 #define LARGO_LEN 2 
 #define POS_PAYLOAD 4
 
 typedef unsigned char uchar; 
 
-status_t readline_ubx(char ** sentencia, bool * eof, FILE * fin);
+bool readline_ubx(uchar ** sentencia, bool * eof, FILE * fin);
+bool get_sentence(uchar * buffer, bool * eof, FILE * fin);
+void load_buffer(uchar * buffer, size_t pos, bool * eof, FILE * fin);
+void fread_grind(void *ptr, size_t size, size_t nmemb, FILE *stream, bool * eof);
 bool checksum(const uchar *buffer);
 
-/*main para testear con NAV-PVT*/
-int main (void){
-	char *sentencia;
-	char buffer2[98];
-	bool eof = false;
-	FILE *fin,
-	     *fout;
-	
-	fin = fopen("UBXtest.txt", "rb");
-	fout = fopen("prueba.txt", "wt");
-
-	while(!eof){
-		readline_ubx(&sentencia, &eof, fin);
-		if(!eof){
-			strncpy(buffer2, sentencia, 98);
-			fprintf(fout, "%s\n",buffer2);
-
-		}
-	}
-
-	fclose(fin);
-	fclose(fout);
-
-	return 1;
-}
-
-/*Si el archivo tiene una sentencia UBX la función la carga en el buffer y devuelve un puntero "sentencia" al principio de la misma. Cuando el archivo se termina y no se encontraron sentencias devuelve eof=true y sentencia=NULL*/
-status_t readline_ubx(char ** sentencia, bool * eof, FILE * fin){
-	static char buffer[STR_LEN];
-	char * ptrsync_char;
-	size_t diferencia;
-
+/*Si el archivo tiene una sentencia UBX la función la carga en el buffer y devuelve un puntero "sentencia" al principio de la misma (no incluye los caracteres de sincronismo). Cuando el archivo se termina y no se encontraron sentencias devuelve eof=true y sentencia=NULL*/
+status_t readline_ubx(uchar ** sentencia, bool * eof, FILE * fin){
+	static uchar buffer[BUFFER_LEN];
 
 	/*punteros no nulos*/
 	if(!sentencia || !eof || !fin){
@@ -58,52 +32,67 @@ status_t readline_ubx(char ** sentencia, bool * eof, FILE * fin){
 
 	*eof = false;
 
-	/*busca los caracteres de sincronismo y guarda su posición en ptrsync_char*/
-	while((ptrsync_char = strstr(buffer, SYNC_CHARS)) == NULL && *eof == false){
-		if(fread(buffer, 1, STR_LEN, fin) != STR_LEN){ /*si no encontró los caracteres vuelve a leer del archivo*/
+	while(get_sentence(buffer, eof, fin)){
+		if(checksum(buffer)){
+			*sentencia = buffer;
+			return ST_OK;
+		}else{
+			/*IMPRIMIR LOG*/
+		}	
+	}
+
+	*sentencia = NULL;
+	return ST_OK;
+}
+
+/*busca una sentencia UBX y la mueve al principio del buffer (no incluye los caracteres de sincronismo)*/
+bool get_sentence(uchar * buffer, bool * eof, FILE * fin){
+	int i = 0;
+
+	/*busca los dos caracteres de sincronismo en el buffer excepto en los dos últimos bytes*/
+	for(i = 0 ; i < BUFFER_LEN-2 ; i++){
+		if(buffer[i] == SYNC_CHAR1){
+			if(buffer[i + 1] == SYNC_CHAR2){
+				/*mueve la sentencia al principio del buffer*/
+				load_buffer(buffer, i + 2, eof, fin);
+				return true;
+			}	
+		}		
+	}
+
+	/*si salió del 'for' y se terminó el archivo es porque no hay más sentencias para leer*/
+	if(eof)
+		return false;
+
+	/*si salió del 'for' y no se terminó el archivo mueve los dos ultimos bytes al principio del buffer y vuelve a empezar*/
+	load_buffer(buffer, BUFFER_LEN-2, eof, fin);
+	return get_sentence(buffer, eof, fin);	
+}
+
+/*borra los bytes que se encuentran antes de la posición 'pos' y carga la misma cantidad al final del buffer leyendo del archivo. 'pos' queda ubicado al principo del buffer*/
+void load_buffer(uchar * buffer, size_t pos, bool * eof, FILE * fin){
+	
+	for(i = 0 ; i < BUFFER_LEN-pos ; i++){
+		buffer[i] = buffer[pos + i];
+	}
+
+	fread_grind(buffer, 1, pos, fin, eof);
+			
+	return;
+}
+
+/*lee del archivo y modifica el puntero 'eof' cuando el archivo se termina*/
+void fread_grind(void *ptr, size_t size, size_t nmemb, FILE *stream, bool * eof){
+	if(fread(ptr, size, nmemb, stream) != nmemb){ 
 			if (ferror(fin)){
 				/*IMPRIMIR LOG*/
 			}
 			if(feof(fin)){
-    				*eof = true; /*después de esta asignación busca los caracteres de sincronismo una vez más antes de salir del while*/
-				/*IMPRIMIR LOG*/
-				break;
-			} 
-		}
-	}
-
-	/*si salió del while y el puntero es NULL es porque se terminó el archivo y no hay más caracteres de sincronismo cargados en el vector 'buffer'*/
-	if(!ptrsync_char){
-		*sentencia = NULL;
-    		return ST_OK;
-	}
-
-	/*mueve el puntero hacia un lugar después de los caracteres de sincronismo*/
-	ptrsync_char += SIZE_OF_SYNC_CHARS;
-
-	/*calcula el espacio inicial del vector donde no se encontraron caracteres de sincronismo*/
-	diferencia = STR_LEN - sizeof(ptrsync_char);
-
-	/*mueve la sentencia al principio del vector 'buffer' ingresado como parámetro*/
-	strcpy(buffer, ptrsync_char);
-
-	/*completa el espacio sobrante al final del vector 'buffer' leyendo del archivo*/
-	if(fread(buffer + (STR_LEN - diferencia), 1, diferencia, fin) != diferencia){
-			if (ferror(fin)){
+    				*eof = true; 
 				/*IMPRIMIR LOG*/
 			}
-			if(feof(fin)){
-    				*eof = true;
-				/*IMPRIMIR LOG*/
-			} 
 	}
-
-    if (checksum((uchar *) buffer)){ /*cast a uchar para llamar a la función checksum que trabaja con operadores de bits*/
-    	*sentencia = buffer;
-    	return ST_OK; 
-    }
-    	
-   return readline_ubx(sentencia, eof, fin); /*si el checksum no concuerda sigue buscando caracteres de sincronismo desde el lugar siguiente a dónde encontro los anteriores. No se pierde información de posibles sentencias válidas incluidas en el espacio que ocupaba la sentencia inválida. Recursividad de cola y todo el mundo contento*/
+	return;
 }
 
 /*calcula el checksum*/
@@ -120,7 +109,7 @@ bool checksum(const uchar *buffer){
 	}
 
 	/*si el largo leído es mayor a la máxima longitud de una sentencia UBX hay un error en la sentencia*/
-	if(largo > STR_LEN)
+	if(largo > BUFFER_LEN)
 		/*IMPRIMIR LOG*/
 		return false;
 
