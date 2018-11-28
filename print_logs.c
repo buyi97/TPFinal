ac45e22511517fd91fb664828c8709260f8fff6c
patovermal <44077261+patovermal@users.noreply.github.com>
2018-11-27 21:59:45 -0300

    Add files via upload

--- print_logs.c
+++ print_logs.c
@@ -6,70 +6,66 @@
 * el mensaje correspondiente en el archivo indicado.
 */
 
-#include <stdio.h>
+#include "logs.h"
 
-#define MSJ_ERR_OPEN_FILE "No se pudo abrir el archivo"
-#define MSJ_ERR_INV_CHKSUM "El checksum no concuerda"
-#define MSJ_ERR_INV_LONG_UBX "El mensaje UBX tiene un largo inválido"
-#define MSJ_ERR_INV_NMEA "El mensaje NMEA está mal formado"
-
-#define MSJ_WARN_ID_DESC "No se reconoce el ID"
-#define MSJ_WARN_FIX_INV "Fix inválido"
-#define MSJ_WARN_FULL_LIST "La lista esta llena. Se descarta el mensaje más viejo"
-
-#define MSJ_DB_BYTES_SYNC "Buscando bytes de sincronismo"
-#define MSJ_DB_ID_DETECT "ID identificado: " //para poner despues que ID
-#define MSJ_DB_MSJ_DET "Se recolectó un mensaje"
-#define MSJ_DB_MSJ_UP "Se cargó un mensaje en la lista"
-
-//typedef enum {"error", "warn", "debug"} log_t;
-void print_logs (status_t logs, FILE *flogs) {
+void print_logs (log_t logs/*, FILE *stdout*/) {
 
     switch(logs) {
 
         /* Errores (ERR) */
-        case ST_ERR_OPEN_FILE:
-            fprintf(flogs, "%s\n", MSJ_ERR_OPEN_FILE);
+        case ERR_OPEN_FILE:
+            fprintf(stdout, "%s\n", MSJ_ERR_OPEN_FILE);
 
-        case ST_ERR_INV_CHKSUM:
-            fprintf(flogs, "%s\n", MSJ_ERR_INV_CHKSUM);
+        case ERR_INV_CHKSUM:
+            fprintf(stdout, "%s\n", MSJ_ERR_INV_CHKSUM);
             break;
 
-        case ST_ERR_INV_LONG_UBX:
-            fprintf(flogs, "%s\n", MSJ_ERR_INV_LONG_UBX);
+        case ERR_INV_LONG_UBX:
+            fprintf(stdout, "%s\n", MSJ_ERR_INV_LONG_UBX);
             break;
 
-        case ST_ERR_INV_NMEA:
-            fprintf(flogs, "%s\n", MSJ_ERR_INV_NMEA);
+        case ERR_INV_NMEA:
+            fprintf(stdout, "%s\n", MSJ_ERR_INV_NMEA);
             break;
         /* Warnings (WARN) */
-        case ST_WARN_ID_DESC:
-            fprintf(flogs, "%s\n", MSJ_WARN_ID_DESC);
+        case WARN_ID_DESC:
+            fprintf(stdout, "%s\n", MSJ_WARN_ID_DESC);
             break;
 
-        case ST_WARN_FIX_INV:
-            fprintf(flogs, "%s\n", MSJ_WARN_FIX_INV);
+        case WARN_FIX_INV:
+            fprintf(stdout, "%s\n", MSJ_WARN_FIX_INV);
             break;
 
-        case ST_WARN_FULL_LIST:
-            fprintf(flogs, "%s\n", MSJ_WARN_FULL_LIST);
+        case WARN_FULL_LIST:
+            fprintf(stdout, "%s\n", MSJ_WARN_FULL_LIST);
             break;
 
         /* Debug (DB) */
-        case ST_DB_BYTES_SYNC:
-            fprintf(flogs, "%s\n", MSJ_DB_BYTES_SYNC);
+        case DB_BYTES_SYNC:
+            fprintf(stdout, "%s\n", MSJ_DB_BYTES_SYNC);
             break;
 
-        case ST_DB_ID_DETECT:
-            fprintf(flogs, "%s\n", MSJ_DB_ID_DETECT);
+        case DB_ID_DETECT:
+            fprintf(stdout, "%s\n", MSJ_DB_ID_DETECT);
             break;
 
-        case ST_DB_MSJ_DET:
-            fprintf(flogs, "%s\n", MSJ_DB_MSJ_DET);
+        case DB_MSJ_DET:
+            fprintf(stdout, "%s\n", MSJ_DB_MSJ_DET);
             break;
 
-        case ST_DB_MSJ_UP:
-            fprintf(flogs, "%s\n", MSJ_DB_MSJ_UP);
+        case DB_MSJ_UP:
+            fprintf(stdout, "%s\n", MSJ_DB_MSJ_UP);
             break;
     }
 }
+
+int main (void) {
+    log_t log_ = DB_MSJ_UP;
+    print_logs(log_);
+
+}
+
+/*
+CABLEVISION IAIA
+20043845307
+*/
